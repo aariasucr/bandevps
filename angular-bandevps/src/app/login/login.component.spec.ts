0e49908dc2af04cd06acf53e5e53b4b3044138e0
José Fernando Quirós Montero <jfqm26@gmail.com>
2020-05-25 12:06:06 -0600

    BAN-22 pruebas para componentes genéricos y header

--- angular-bandevps/src/app/login/login.component.spec.ts
+++ angular-bandevps/src/app/login/login.component.spec.ts
@@ -1,6 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from './login.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {environment} from '../../environments/environment';
+import {NgForm} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +14,14 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule,
+        RouterTestingModule
+      ],
+      declarations: [LoginComponent, NgForm]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
