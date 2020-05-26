f9ecbe387650c47587a1dedb46a0d5aabbcd13d5
DilianBM <40511225+DilianBM@users.noreply.github.com>
2020-05-25 23:42:57 -0600

    BAN-8 se agregaron las pruebas

--- angular-bandevps/src/app/seguros/seguros.component.spec.ts
+++ angular-bandevps/src/app/seguros/seguros.component.spec.ts
@@ -1,16 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {environment} from '../../environments/environment';
+import {SegurosComponent} from './seguros.component';
+import {SegurosService} from '../shared/seguros.service';
 
-import { SegurosComponent } from './seguros.component';
-
-describe('SegurosComponent', () => {
+fdescribe('SegurosComponent', () => {
   let component: SegurosComponent;
   let fixture: ComponentFixture<SegurosComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SegurosComponent ]
-    })
-    .compileComponents();
+      imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule
+      ],
+      declarations: [SegurosComponent],
+      providers: [SegurosService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +30,13 @@ describe('SegurosComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render insurances titles in h2 tag', () => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Seguro médico');
+    //expect(compiled.querySelector('h2').textContent).toContain('Seguro viajero');
+    //expect(compiled.querySelector('h2').textContent).toContain('Seguro de vida');
   });
 });
--- angular-bandevps/src/app/shared/seguros.service.spec.ts
+++ angular-bandevps/src/app/shared/seguros.service.spec.ts
@@ -1,9 +1,20 @@
-import { TestBed } from '@angular/core/testing';
-
-import { SegurosService } from './seguros.service';
+import {TestBed} from '@angular/core/testing';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {environment} from '../../environments/environment';
+import {SegurosService} from './seguros.service';
 
 describe('SegurosService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule
+      ]
+    })
+  );
 
   it('should be created', () => {
     const service: SegurosService = TestBed.get(SegurosService);
