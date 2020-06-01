af915c54edfae54bde4fb822e5d6e1da2f05dd0c
DilianBM <40511225+DilianBM@users.noreply.github.com>
2020-05-31 18:51:01 -0600

    BAN-17 modificaciones en el componente editar información

--- angular-bandevps/src/app/edit-information/edit-information.component.ts
+++ angular-bandevps/src/app/edit-information/edit-information.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {NgForm, FormGroup, FormControl} from '@angular/forms';
+import {NotificationService} from '../shared/notification.service';
+import {UserService} from '../shared/user.service';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {Subscription} from 'rxjs';
+import { UserData } from '../shared/models';
 
 @Component({
   selector: 'app-edit-information',
   templateUrl: './edit-information.component.html',
   styleUrls: ['./edit-information.component.css']
 })
-export class EditInformationComponent implements OnInit {
-
-  constructor() { }
+export class EditInformationComponent implements OnInit, OnDestroy {
+  userInfoForm: FormGroup;
+  private subscription: Subscription;
+  private userData:UserData
+  constructor(
+    private notificationServie: NotificationService,
+    //private spinnerService: SpinnerService,
+    private userService: UserService,
+    private firebaseAuth: AngularFireAuth
+  ) {}
 
   ngOnInit() {
+    this.userInfoForm = new FormGroup({
+      phoneNumber: new FormControl(''),
+      address: new FormControl(''),
+      occupation: new FormControl('')
+    });
+    this.subscription = this.userService.statusChange.subscribe((userData) => {
+      if (userData) {
+        this.userData=userData;
+        console.log(userData);
+      }
+    });
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+  onSubmitUserIdForm() {
+    console.log('userIdForm', this.userInfoForm.get('address').value);
+  }
 }
--- angular-bandevps/src/app/shared/edit-info.service.ts
+++ angular-bandevps/src/app/shared/edit-info.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AngularFireDatabase } from '@angular/fire/database/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditInfoService {
-  private dbPath = '/insurances';
-  items: Observable<any[]>;
+  private dbPath = '/users_info';
+ // items: Observable<any[]>;
+  items: AngularFireList<any> = null;
   constructor(private db: AngularFireDatabase) {
-    this.items = db.list(this.dbPath).valueChanges();
+    this.items = db.list(this.dbPath);
   }
 
+  updateCustomer(key: string, value: any): Promise<void> {
+    return this.items.set(key, value);
+  }
  // updateCustomer(key: string, value: any): Promise<void> {
    // return this.items.update(key, value);
  // }
