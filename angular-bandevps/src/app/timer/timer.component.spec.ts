0ea382a8adddfd2964c1d17ab55809487fae4f6b
Jose Fernando Quiros Montero <jfqm26@gmail.com>
2020-06-28 16:45:30 -0600

    agrega correcciones, mejoras y pruebas

--- angular-bandevps/src/app/timer/timer.component.spec.ts
+++ angular-bandevps/src/app/timer/timer.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed, fakeAsync} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {TimerComponent} from './timer.component';
 import {StoreModule} from '@ngrx/store';
 import {reducers} from '../state/reducers';
--- angular-bandevps/src/app/ubicaciones/ubicaciones.component.ts
+++ angular-bandevps/src/app/ubicaciones/ubicaciones.component.ts
@@ -3,7 +3,6 @@ import {LocationsService} from '../shared/locations.service';
 import {Observable, Subscription} from 'rxjs';
 import {MapInfoWindow, MapMarker} from '@angular/google-maps';
 
-
 @Component({
   selector: 'app-ubicaciones',
   templateUrl: './ubicaciones.component.html',
@@ -17,34 +16,28 @@ export class UbicacionesComponent implements OnInit, OnDestroy {
   latValue = 9.951309;
   lngValue = -84.046914;
 
-
-
   center = {lat: this.latValue, lng: this.lngValue};
   zoom = 8;
   display?: google.maps.LatLngLiteral;
   markerOptions = {draggable: false};
   markerPositions: google.maps.LatLngLiteral[] = [];
-  locationsSuscription:Subscription
+  locationsSuscription: Subscription;
   constructor(private locationsService: LocationsService) {}
 
-
   ngOnInit() {
-  this.locationsSuscription=this.locationsService.getLocationsList().subscribe((result)=>{
-    console.log(result);
-    result.forEach((elemt)=>{
-      this.markerPositions.push({lat:elemt.post_latitude,lng:elemt.post_longitude});
-    })
+    this.locationsSuscription = this.locationsService.getLocationsList().subscribe((result) => {
+      console.log(result);
+      result.forEach((elemt) => {
+        this.markerPositions.push({lat: elemt.post_latitude, lng: elemt.post_longitude});
+      });
     });
-
   }
 
   ngOnDestroy(): void {
-    if(!!this.locationsSuscription){
-      this.locationsSuscription.unsubscribe()
+    if (!!this.locationsSuscription) {
+      this.locationsSuscription.unsubscribe();
     }
   }
-
-
 
   move(event: google.maps.MouseEvent) {
     this.display = event.latLng.toJSON();
