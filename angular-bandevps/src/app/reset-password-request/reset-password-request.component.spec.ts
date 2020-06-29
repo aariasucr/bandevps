96a77a7c8a3643c85eed3b97454ccac0a8792909
Jose Fernando Quiros Montero <jfqm26@gmail.com>
2020-06-28 19:09:13 -0600

    pruebas para reset password request

--- angular-bandevps/src/app/reset-password-request/reset-password-request.component.spec.ts
+++ angular-bandevps/src/app/reset-password-request/reset-password-request.component.spec.ts
@@ -1,33 +1,81 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed, tick, fakeAsync} from '@angular/core/testing';
 
 import {ResetPasswordRequestComponent} from './reset-password-request.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {AngularFireModule} from '@angular/fire';
-import {AngularFireDatabaseModule} from '@angular/fire/database';
-import {AngularFireAuthModule} from '@angular/fire/auth';
-import {environment} from '../../environments/environment';
+import {ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
+import {AngularFireAuth} from '@angular/fire/auth';
 import {ToastrModule} from 'ngx-toastr';
 import {ModalModule} from 'ngx-bootstrap/modal';
 import {Router} from '@angular/router';
+import {UserService} from '../shared/user.service';
+import {SpinnerService} from '../shared/spinner.service';
+import {NgxSpinnerModule} from 'ngx-spinner';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NotificationService} from '../shared/notification.service';
 
 describe('ResetPasswordRequestComponent', () => {
   let component: ResetPasswordRequestComponent;
   let fixture: ComponentFixture<ResetPasswordRequestComponent>;
 
   const mockRouter: any = {navigate() {}};
+  const mockUserDataNoRegistrado = {
+    registered: false,
+    id: '999999999'
+  };
+
+  const mockUserDataRegistrado = {
+    registered: true,
+    id: '102340567',
+    email: 'test@test.com'
+  };
+
+  const mockAngularFireAuth: any = {
+    sendPasswordResetEmail() {
+      return Promise.resolve(true);
+    }
+  };
+  const mockUserService: any = {
+    getUserDataFromFirebaseWithId(id) {
+      if (id === '1') {
+        return Promise.reject('INVALID_ID');
+      } else if (id === '102340567') {
+        return Promise.resolve(mockUserDataRegistrado);
+      } else if (id === '999999999') {
+        return Promise.resolve(mockUserDataNoRegistrado);
+      }
+    }
+  };
+
+  let mockUserIdForm: FormGroup;
+  // let checkUserCanResetPasswordSpy: jasmine.Spy;
+  let auth: AngularFireAuth;
+  let notification: NotificationService;
+  let resetPasswordSpy: jasmine.Spy;
+  let errorMessageSpy: jasmine.Spy;
+
+  const userPasswordReset = () => {
+    component.userIdForm = mockUserIdForm;
+    component.onSubmit();
+    tick();
+    fixture.detectChanges();
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         ReactiveFormsModule,
-        AngularFireModule.initializeApp(environment.firebaseConfig),
-        AngularFireDatabaseModule,
-        AngularFireAuthModule,
+        NgxSpinnerModule,
         ToastrModule.forRoot(),
-        ModalModule.forRoot()
+        ModalModule.forRoot(),
+        BrowserAnimationsModule
       ],
       declarations: [ResetPasswordRequestComponent],
-      providers: [{provide: Router, useValue: mockRouter}]
+      providers: [
+        {provide: Router, useValue: mockRouter},
+        {provide: UserService, useValue: mockUserService},
+        {provide: AngularFireAuth, useValue: mockAngularFireAuth},
+        SpinnerService,
+        NotificationService
+      ]
     }).compileComponents();
   }));
 
@@ -35,9 +83,43 @@ describe('ResetPasswordRequestComponent', () => {
     fixture = TestBed.createComponent(ResetPasswordRequestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    auth = fixture.debugElement.injector.get(AngularFireAuth);
+    resetPasswordSpy = spyOn(auth, 'sendPasswordResetEmail');
+    notification = fixture.debugElement.injector.get(NotificationService);
+    errorMessageSpy = spyOn(notification, 'showErrorMessage');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not reset password for invalid user id', fakeAsync(() => {
+    mockUserIdForm = new FormGroup({
+      id: new FormControl('1')
+    });
+    userPasswordReset();
+    tick(10000);
+    expect(resetPasswordSpy).not.toHaveBeenCalled();
+    expect(errorMessageSpy).toHaveBeenCalled();
+  }));
+
+  it('should not reset password for unregistered user', fakeAsync(() => {
+    mockUserIdForm = new FormGroup({
+      id: new FormControl('999999999')
+    });
+    userPasswordReset();
+    tick(10000);
+    expect(resetPasswordSpy).not.toHaveBeenCalled();
+    expect(errorMessageSpy).toHaveBeenCalled();
+  }));
+
+  it('should reset password for registered user', fakeAsync(() => {
+    mockUserIdForm = new FormGroup({
+      id: new FormControl('102340567')
+    });
+    userPasswordReset();
+    tick(10000);
+    expect(resetPasswordSpy).toHaveBeenCalled();
+  }));
 });
